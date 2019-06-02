57bf4d069054a9c9dbf17ec2fae3935b7295bfc2
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-06-02 15:33:50 -0500

    Got monkey and chicken to print

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+  // Divide the current number by 2, and check if the remainder is 0
+
+  if (currentNumber % 5 === 0) {
+    console.log(currentNumber);
+    document.write(" Chicken"); 
+  }
+  if (currentNumber % 7 === 0) {
+    console.log(currentNumber);
+    document.write(" Monkey");
+  }
+}
